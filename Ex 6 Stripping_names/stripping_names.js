891e510264e0024fb85a5137e09a56efde5c565e
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-02-28 15:01:34 +0500

    Add files via upload

---
+++ Ex 6 Stripping_names/stripping_names.js
@@ -0,0 +1,6 @@
+"use strict";
+// Exercise 6 Stripping Names//
+let person_Name = ' \n\t\ Nimrah Khan\t\n';
+console.log(person_Name);
+let stripped = person_Name.trim();
+console.log(stripped);
---
+++ Ex 6 Stripping_names/stripping_names.ts
@@ -0,0 +1,8 @@
+ // Exercise 6 Stripping Names//
+
+ let person_Name : string = ' \n\t\ Nimrah Khan\t\n' ; 
+ console.log(person_Name) ;
+
+ let stripped : string = person_Name.trim() 
+
+ console.log(stripped) ;
