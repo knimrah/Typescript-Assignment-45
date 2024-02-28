d4159ba0182d1addc84f6dda8477e26220691ca5
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-02-28 14:58:28 +0500

    Add files via upload

---
+++ Ex 5 Famous_Quote_2/famous_Quote.js
@@ -0,0 +1,4 @@
+"use strict";
+let author = 'Eleanor Roosevelt';
+let quote = '"The future belongs to those who believe in the beauty of their dreams."';
+console.log(author, 'once said', quote);
---
+++ Ex 5 Famous_Quote_2/famous_Quote.ts
@@ -0,0 +1,8 @@
+
+//Exercise 5 Famous Quote//
+
+let author : string = 'Eleanor Roosevelt' ;
+let quote : string = '"The future belongs to those who believe in the beauty of their dreams."' ;
+
+console.log( author ,'once said', quote) ;
+
