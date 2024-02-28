821b1484a741743f621299a0f4af65ee2cf7f6f8
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-02-28 15:39:18 +0500

    Add files via upload

---
+++ Ex 20 List_Items/empty_Array.js
@@ -0,0 +1,9 @@
+"use strict";
+//Ex 20 Empty Array List Items//
+let empty_Array = ['English', 'Urdu', 'Arabic'];
+let list_items = [];
+list_items.push('Dutch');
+list_items.push('French');
+list_items.push('German');
+list_items.push('French');
+console.log(list_items);
---
+++ Ex 20 List_Items/empty_Array.ts
@@ -0,0 +1,12 @@
+//Ex 20 Empty Array List Items//
+
+let empty_Array: string[] = [ 'English' , 'Urdu' , 'Arabic'] ;
+
+let list_items : string[] = [] ;
+
+list_items.push('Dutch') ;
+list_items.push('French') ;
+list_items.push('German') ;
+list_items.push('French') ;
+
+console.log(list_items) ;
