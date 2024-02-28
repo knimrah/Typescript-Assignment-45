89835d0260ca758231929fef9b879bcb2d43c7fe
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-02-28 15:16:23 +0500

    Add files via upload

---
+++ Ex 13 Your_Own _Array/fav_transport.js
@@ -0,0 +1,6 @@
+"use strict";
+//Exercise 13 Favourite Mode of Transprt//
+let transport = ['Toyota Fortuner Legender', 'Toyota Hilux Vigo', 'Sports Bike', 'Suzuki Swift'];
+for (let i = 0; i < transport.length; i++) {
+    console.log('I would like to own a  ' + transport[i]);
+}
---
+++ Ex 13 Your_Own _Array/fav_transport.ts
@@ -0,0 +1,7 @@
+ //Exercise 13 Favourite Mode of Transprt//
+
+ let transport : string[] = [ 'Toyota Fortuner Legender' , 'Toyota Hilux Vigo' , 'Sports Bike' , 'Suzuki Swift'] ;
+
+for( let i=0; i<transport.length ; i++) {
+    console.log ('I would like to own a  ' + transport[i]) ;
+}
