a04a3aa166b9c2e29f04b968839974fdc499a89a
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-02-28 14:51:54 +0500

    Add files via upload

---
+++ Ex 2 personal_message/personal_message.js
@@ -0,0 +1,5 @@
+"use strict";
+//Exercise 2 Personal Message
+let person_Name = ('Eric');
+let message = (`Hello ${person_Name} ! would you like to learn Python today?`);
+console.log(message);
---
+++ Ex 2 personal_message/personal_message.ts
@@ -0,0 +1,5 @@
+//Exercise 2 Personal Message
+
+let person_Name: string = ('Eric');
+let message: string = (`Hello ${person_Name} ! would you like to learn Python today?`);
+console.log(message);
