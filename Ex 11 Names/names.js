9a2266a12f4b2b438a33576a93d35183def98a8b
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-02-28 15:11:02 +0500

    Add files via upload

---
+++ Ex 11 Names/names.js
@@ -0,0 +1,6 @@
+"use strict";
+//Exercise 11 Name of friends
+let friends = ['Munaza', 'Alveena', 'Arooba', 'Huma', 'Nimrah'];
+for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+}
---
+++ Ex 11 Names/names.ts
@@ -0,0 +1,7 @@
+//Exercise 11 Name of friends
+
+let friends : string[] = ['Munaza' , 'Alveena' , 'Arooba' , 'Huma' , 'Nimrah'] ;
+for( let i=0 ; i<friends.length ;i++ ) {
+    console.log(friends [i])  ;
+}
+
