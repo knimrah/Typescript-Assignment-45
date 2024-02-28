b7e16204c209bca65e414128cd76fcb1643fd59a
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-02-28 14:55:50 +0500

    Add files via upload

---
+++ Ex 4 Famous_Quote/famous_quote1.js
@@ -0,0 +1,3 @@
+"use strict";
+//Exercise 4 Favourite Quote//
+console.log('Mae West once said "You only live once, but if you do it right, once is enough." ');
---
+++ Ex 4 Famous_Quote/famous_quote1.ts
@@ -0,0 +1,4 @@
+
+//Exercise 4 Favourite Quote//
+
+console.log('Mae West once said "You only live once, but if you do it right, once is enough." ')
