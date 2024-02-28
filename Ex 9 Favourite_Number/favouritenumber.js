9c5d2290d51400b65fccf55eb3ded651872c42a9
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-02-28 15:07:51 +0500

    Add files via upload

---
+++ Ex 9 Favourite_Number/favouritenumber.js
@@ -0,0 +1,4 @@
+"use strict";
+//Exercise 9 Favourite Number//
+let Favourite_number = 5;
+console.log('My favourite number is', Favourite_number);
---
+++ Ex 9 Favourite_Number/favouritenumber.ts
@@ -0,0 +1,6 @@
+
+//Exercise 9 Favourite Number//
+
+let Favourite_number : number = 5
+
+console.log('My favourite number is', Favourite_number) ;
