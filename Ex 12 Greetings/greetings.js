637bb5d074e96e5bc6cb4a2fde2beb95c53c19cf
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-02-28 15:13:06 +0500

    Add files via upload

---
+++ Ex 12 Greetings/greetings.js
@@ -0,0 +1,7 @@
+"use strict";
+//Exercise 12 Name of friends
+let friends = ['Munaza', 'Alveena', 'Arooba', 'Huma', 'Nimrah'];
+const message = 'hello how are you  ';
+for (let i = 0; i < friends.length; i++) {
+    console.log(message + friends[i] + '?');
+}
---
+++ Ex 12 Greetings/greetings.ts
@@ -0,0 +1,9 @@
+//Exercise 12 Name of friends
+
+let friends : string[] = ['Munaza' , 'Alveena' , 'Arooba' , 'Huma' , 'Nimrah'] ;
+
+const message : string = 'hello how are you  ' ;
+
+for( let i=0 ; i<friends.length ;i++ ) {
+    console.log (message + friends [i] + '?') ;
+}
