14de4b5fcfae772ce8e15009fa846e676bf25a06
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-03-01 23:55:23 +0500

    Add files via upload

---
+++ Ex 26 Alien Color 2/Aliencolor2.js
@@ -0,0 +1,16 @@
+"use strict";
+//Exercise 26 Alien Color 2
+let alien_Color = 'Green';
+if (alien_Color === 'Green') {
+    console.log('player just earned a 5 points for shooting the alien.');
+}
+else {
+    console.log('player just earned 10 points.');
+}
+alien_Color = 'Blue';
+if (alien_Color === 'Green') {
+    console.log('player just earned a 5 points for shooting the alien.');
+}
+else {
+    console.log('player just earned 10 points.');
+}
---
+++ Ex 26 Alien Color 2/Aliencolor2.ts
@@ -0,0 +1,24 @@
+//Exercise 26 Alien Color 2
+
+//Runs If block
+
+let alien_Color = 'Green';
+
+if (alien_Color === 'Green') {
+    console.log('player just earned a 5 points for shooting the alien.')
+
+}
+else {
+    console.log('player just earned 10 points.')
+}
+
+//Runs Else block
+alien_Color = 'Blue';
+
+if (alien_Color === 'Green') {
+    console.log('player just earned a 5 points for shooting the alien.')
+
+}
+else {
+    console.log('player just earned 10 points.')
+}
