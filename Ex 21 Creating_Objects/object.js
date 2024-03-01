50ac546473a5f11e60a720b7b405f46cdfb8a536
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-03-01 19:36:32 +0500

    Add files via upload

---
+++ Ex 21 Creating_Objects/object.js
@@ -0,0 +1,22 @@
+"use strict";
+//Exercise 21 Objects//
+let samsung = {
+    name: 'Samsung Galaxy A25',
+    price: 'Rs. 90,499',
+    Ram: '6 GB',
+    OS: 'Android 14 OS',
+    weight: '195 g',
+    size: '6.5 inches',
+    Torch: true
+};
+let iPhone = {
+    name: 'Apple iPhone 14 Pro Max',
+    price: 'Rs. 565,999',
+    Ram: '6 GB',
+    OS: '	IOS 16',
+    weight: '240 g',
+    size: '6.7 inches',
+    Torch: true
+};
+console.log(samsung);
+console.log(iPhone);
---
+++ Ex 21 Creating_Objects/object.ts
@@ -0,0 +1,46 @@
+//Exercise 21 Objects//
+
+
+interface samsung {
+    name: String
+    price: number
+    Ram: string
+    OS: string
+weight: string
+    size: string
+    Torch: boolean
+}
+
+let samsung = {
+    name: 'Samsung Galaxy A25' ,
+    price: 'Rs. 90,499' ,
+    Ram: '6 GB' ,
+    OS: 'Android 14 OS' ,
+    weight : '195 g' ,
+    size: '6.5 inches' ,
+    Torch: true
+}
+
+console.log(samsung) ;
+
+interface iPhone {
+    name: String
+    price: number
+    Ram: string
+    OS: string
+    weight: string
+    size: string
+    Torch: boolean
+}
+
+let iPhone = {
+    name: 'Apple iPhone 14 Pro Max' ,
+    price: 'Rs. 565,999' ,
+    Ram: '6 GB' ,
+    OS: '	IOS 16' ,
+    weight : '240 g' ,
+    size: '6.7 inches' ,
+    Torch: true
+}
+
+console.log(iPhone) ;
