99a82868ed79b6efdc3cad2ebeec57b8b79e2d5e
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-03-01 23:42:08 +0500

    Add files via upload

---
+++ Ex 25 Alien Colors/AlienColors.ts
@@ -0,0 +1,18 @@
+//Exercise 25 Alien Colors
+
+//Pass Test
+
+let alien_Color = 'green' ;
+
+if ( alien_Color === 'green') {
+    console.log('Player just earned a 5 points! ');
+
+}
+//fail Test
+
+alien_Color = 'Yellow' ;
+
+if ( alien_Color === 'green') {
+    console.log('Player just earned a 5 points! ');
+
+}
