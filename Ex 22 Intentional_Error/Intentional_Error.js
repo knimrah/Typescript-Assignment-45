fa4027ea0e8d4289dc3a83b4aefb9b4c590ff879
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-03-01 22:00:35 +0500

    Add files via upload

---
+++ Ex 22 Intentional_Error/Intentional_Error.js
@@ -0,0 +1,7 @@
+"use strict";
+//Exercise 22 Intentional Error//
+let error = ['earth', 'Sun', 'Moon', 'sky'];
+//there are 4 members in an array and we try to call the six member which cannot be in our array
+console.log(error[6]);
+//let fix this error now//
+console.log(error);
---
+++ Ex 22 Intentional_Error/Intentional_Error.ts
@@ -0,0 +1,10 @@
+//Exercise 22 Intentional Error//
+
+let error : string[] = ['earth' , 'Sun' , 'Moon' , 'sky'] ;
+
+//there are 4 members in an array and we try to call the six member which cannot be in our array
+console.log(error[6]) ;
+
+//let fix this error now//
+
+console.log(error) ;
