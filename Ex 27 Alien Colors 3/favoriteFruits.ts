d89ca3fe72870504f0717f8220efc68c0942af96
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-03-03 18:40:52 +0500

    Add files via upload

---
+++ Ex 27 Alien Colors 3/favoriteFruits.ts
@@ -0,0 +1,20 @@
+//Exercise 28 Favourite Fruits Array
+
+let favorite_Fruit : string[] = ['Apple', 'Orange' , 'Mango'] ;
+
+
+if (favorite_Fruit.includes('Apple')) {
+    console.log('I really like Apple!')
+}
+if (favorite_Fruit.includes('Orange')) {
+    console.log('I really like Orange!')
+}
+if (favorite_Fruit.includes('Banana')) {
+    console.log('I really like Banana!')
+}
+if (favorite_Fruit.includes('Mango')) {
+    console.log('I really like Mango!')
+}
+if (favorite_Fruit.includes('Kiwi')) {
+    console.log('I really like Kiwi!')
+}
