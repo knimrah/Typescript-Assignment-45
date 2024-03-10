e2516af8409c0fd23fb348c792a2eae5852bb430
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-03-10 18:37:20 +0500

    Add files via upload

---
+++ Ex 31 No Users/noUsers.js
@@ -0,0 +1,10 @@
+"use strict";
+//Exercise 31 No users
+let noUsers = ['Admin', 'William', 'Alexander', 'tervor', 'Steven'];
+if (noUsers.length === 0) {
+    console.log('We need to find some users.');
+}
+else
+    noUsers = [];
+console.log('we need to find some users.');
+console.log(' All users have been removed ' + noUsers.length);
