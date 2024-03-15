e27bfaa5318129a1e1f2cd9a8ba7150dc0830a7c
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-03-15 21:39:42 +0500

    Add files via upload

---
+++ Ex 33 Ordinal Number/ordinalNumber.js
@@ -0,0 +1,16 @@
+"use strict";
+//Exercise 33 Ordinal Numbers
+let Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+Numbers.forEach(number => {
+    let suffix = 'th';
+    if (number === 1) {
+        suffix = 'st';
+    }
+    else if (number === 2) {
+        suffix = 'nd';
+    }
+    else if (number === 3) {
+        suffix = 'rd';
+    }
+    console.log(`${number}${suffix}`);
+});
