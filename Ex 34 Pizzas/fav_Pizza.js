aeb4b5920a9dbe3c4beab1ad0d8513d83060855c
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-03-15 21:41:00 +0500

    Add files via upload

---
+++ Ex 34 Pizzas/fav_Pizza.js
@@ -0,0 +1,7 @@
+"use strict";
+//Exercise 34 Favorite Pizza
+let pizzas = ['Chicken Tikka', 'Fajita', 'Margherita'];
+pizzas.forEach(pizza => {
+    console.log(`I really like ${pizza} pizza`);
+});
+console.log('I really love Pizza.');
---
+++ Ex 34 Pizzas/fav_Pizza.ts
@@ -0,0 +1,9 @@
+//Exercise 34 Favorite Pizza
+
+let pizzas : string [] = ['Chicken Tikka' , 'Fajita' , 'Margherita'] ;
+
+pizzas.forEach(pizza => {
+    console.log (`I really like ${pizza} pizza`) ;
+})
+
+console.log('I really love Pizza.')
