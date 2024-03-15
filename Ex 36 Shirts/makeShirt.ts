3e43621fe846f722e167622c08bed0c917ce009a
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-03-15 21:43:48 +0500

    Add files via upload

---
+++ Ex 36 Shirts/makeShirt.ts
@@ -0,0 +1,13 @@
+
+//Exercise 36 Make Shirt
+
+// Write a function called make_shirt() that accepts a size and the text of a message that should
+// be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the
+// message printed on it. Call the function.
+function make_shirt(size:string , message : string) {
+    console.log(`making a ${size} t-shirt with the message ${message} `) ;
+}
+
+make_shirt('medium' , 'Believe in your art.') ;
+make_shirt('large' , 'Good Vibes Only.') ;
+make_shirt('small' , 'Think Outside the Box.') ;
