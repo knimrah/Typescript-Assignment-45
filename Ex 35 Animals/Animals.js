a833597b8b6d21c48f67da4b5d759f3ffa8c0907
Nimrah Khan <158679378+knimrah@users.noreply.github.com>
2024-03-15 21:42:30 +0500

    Add files via upload

---
+++ Ex 35 Animals/Animals.js
@@ -0,0 +1,7 @@
+"use strict";
+//Exercise 35 Animals 
+let animals = ['Cat', 'Dog', 'Rabbit'];
+animals.forEach(animal => {
+    console.log(`A ${animal} would make a great pet.`);
+});
+console.log('Any of these animals would make a great pet!');
---
+++ Ex 35 Animals/Animals.ts
@@ -0,0 +1,9 @@
+//Exercise 35 Animals 
+
+let animals : string [] = ['Cat' , 'Dog' , 'Rabbit'] ;
+
+animals.forEach(animal => {
+    console.log(`A ${animal} would make a great pet.`)
+})
+
+console.log('Any of these animals would make a great pet!') ;
